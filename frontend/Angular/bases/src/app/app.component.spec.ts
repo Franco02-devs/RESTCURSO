736eea42b2369471fe5c9ba46c44d8f72fadadba
Franco <francocahuasoto@gmail.com>
2025-03-19 18:04:25 -0500

    angggg

--- frontend/Angular/bases/src/app/app.component.spec.ts
+++ frontend/Angular/bases/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ElectroTotal' title`, () => {
+  it(`should have the 'bases' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ElectroTotal');
+    expect(app.title).toEqual('bases');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ElectroTotal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bases');
   });
 });
